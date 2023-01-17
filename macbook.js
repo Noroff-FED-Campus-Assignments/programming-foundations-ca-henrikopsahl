dfe9ddd94dd41ecd7b5627c5030804dec9b75374
henrikopsahl <henrikopsahl@hotmail.com>
2023-01-17 19:06:07 +0100

    added macbook.js

---
+++ macbook.js
@@ -0,0 +1,14 @@
+const macbook = {
+    brand: "apple",
+    chip: "m1",
+    year: "2020",
+    screensize: 13,
+    wifi: "true",
+    usb: 4,
+    colours: ["grey", "black", "silver"],
+    ignite: function (){
+        macbook.on = !macbook.on;
+    },
+    };
+
+console.log(macbook);
